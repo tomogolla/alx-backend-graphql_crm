85aa6f23d96dbe5a1a8151abd0194c0aad9fd041
tomogolla <tomogolla637@gmail.com>
2025-08-29 13:55:37 +0300

    extra task

--- crm/tasks.py
+++ crm/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 from datetime import datetime
+import requests
+
 
 logger = logging.getLogger(__name__)
 
